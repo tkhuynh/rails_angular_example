960bdb4590c8094ad0a39e51a41436043fcf8cbe
tkhuynh <thequest2311@yahoo.com>
2016-10-05 16:11:16 -0700

    add controllers

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,25 @@
+class CommentsController < ApplicationController
+	before_action :get_post
+
+	def create
+		comment = @post.comments.create(comment_params)
+		respond_with post, comment
+	end
+
+	def upvote
+		comment = @post.comments.find(params[:id])
+		comment.increment!(:upvotes)
+		respond_with post, comment
+	end
+
+	private
+
+	def get_post
+		@post = Post.find(params[:post_id])
+	end
+
+	def comment_params
+		params.require(:comment).permit(:body)
+	end
+
+end
---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,31 @@
+class PostsController < ApplicationController
+	before_action :get_post, only: [:show, :upvote]
+
+	def index
+		respond_with Post.all
+	end
+
+	def show
+		respond_with @post
+	end
+
+	def create
+		respond_with Post.create(post_params)
+	end
+
+	def upvote
+		@post.increment!(:upvotes)
+		respond_with @post
+	end
+
+	private
+
+	def get_post
+		@post =  Post.find(params[:id])
+	end
+
+	def post_params
+		params.require(:post).permint(:link, :title)
+	end
+
+end
