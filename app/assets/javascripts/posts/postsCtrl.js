5a46dfd191a7dd68209e79113851691509f75e48
tkhuynh <thequest2311@yahoo.com>
2016-10-07 21:27:00 -0700

    add some styling

--- app/assets/javascripts/posts/postsCtrl.js
+++ app/assets/javascripts/posts/postsCtrl.js
@@ -1,19 +1,28 @@
-app.controller('PostCtrl', ['$scope', '$stateParams', 'posts', 'post', function($scope, $stateParams, posts, post) {
-	$scope.post = post;
-	$scope.addComment = function() {
-		if ($scope.body === '') return;
-		posts.addComment(post.id, {
-			body: $scope.body,
-			author: 'user'
-		})
-		.success(function(comment) {
-			$scope.post.comments.push(comment);
-		});
-		$scope.body = '';
-	};
+app.controller('PostCtrl', ['$scope', '$stateParams', 'posts', 'post', '$location',
+	function($scope, $stateParams, posts, post, $location) {
+		$scope.post = post;
+		$scope.addComment = function() {
+			if ($scope.body === '') return;
+			posts.addComment(post.id, {
+					body: $scope.body,
+					author: 'user'
+				})
+				.success(function(comment) {
+					$scope.post.comments.push(comment);
+				});
+			$scope.body = '';
+		};
 
-	$scope.incrementUpvotes = function(comment) {
-		posts.upvoteComment(post, comment);
-	};
+		$scope.clearComment = function() {
+			$scope.body = '';
+		};
 
-}]);
+		$scope.home = function() {
+			$location.path('/home');
+		};
+
+		$scope.incrementUpvotes = function(comment) {
+			posts.upvoteComment(post, comment);
+		};
+	}
+]);
